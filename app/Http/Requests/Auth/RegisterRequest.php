97d2b8c1a0cbecb32e1d4a61b24f614a26801f60
Luwnto <13521760670@163.com>
2021-05-08 11:39:29 +0800

    fix: 优化

--- app/Http/Requests/Auth/RegisterRequest.php
+++ app/Http/Requests/Auth/RegisterRequest.php
@@ -17,6 +17,7 @@ class RegisterRequest extends BaseRequest
             'name' => 'required|max:16',
             'email' => 'required|email|unique:users',
             'password' => 'required|min:6|max:16|confirmed',
+            'openid' => 'sometimes|required|unique:users,openid'
         ];
     }
 
@@ -25,6 +26,8 @@ class RegisterRequest extends BaseRequest
         return [
             'name.required' => '昵称 不能为空',
             'name.max' => '昵称 不能超过16个字符',
+            'openid.required' => 'openid 不能为空',
+            'openid.unique' => 'openid 已绑定其他用户',
         ];
     }
 }
